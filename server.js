cf8c31c9a388cbbc16e25972555813406bc0339a
Protonsint <protonsint@gmail.com>
2016-08-07 19:28:01 +0530

    src

---
+++ server.js
@@ -0,0 +1,74 @@
+// Check the configuration file for more details
+var config = require('./config');
+
+
+var express = require('express');
+var app = module.exports = express.createServer();
+socket = require('socket.io');
+
+var io = socket.listen(app);
+
+// Configuration
+app.configure(function(){
+  app.use("/chat", express.static(__dirname + '/chat'));
+  app.use("/admin", express.static(__dirname + '/admin'));
+
+});
+
+/* var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Express server listening on port %d", app.address().port);
+});
+ */
+ 
+ //Tell the server to listen for incoming connections
+// Both port and ip are needed for the OpenShift, otherwise it tries 
+// to bind server on IP 0.0.0.0 (or something) and fails
+ app.listen(config.serverport, config.serverip, function() {
+  console.log("Server running @ http://" + config.serverip + ":" + config.serverport);
+});/* */
+
+ 
+var usernames = {};
+
+var rooms = ['Lobby'];
+
+io.sockets.on('connection', function(socket) {
+    socket.on('adduser', function(username) {
+        socket.username = username;
+        socket.room = 'Lobby';
+        usernames[username] = username;
+        socket.join('Lobby');
+        socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
+        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'Lobby');
+    });
+
+    socket.on('create', function(room) {
+        rooms.push(room);
+        socket.emit('updaterooms', rooms, socket.room);
+    });
+
+    socket.on('sendchat', function(data) {
+        io.sockets["in"](socket.room).emit('updatechat', socket.username, data);
+    });
+
+    socket.on('switchRoom', function(newroom) {
+        var oldroom;
+        oldroom = socket.room;
+        socket.leave(socket.room);
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+        socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
+    });
+
+    socket.on('disconnect', function() {
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
+ });
